c70c54ead92e7233ec05f3bca35a1aaee9c03ae4
Gabrijel Anic <gabrijel.web@gmail.com>
2016-06-22 23:56:35 +0200

    Reducers and Action setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-console.log('hello world');
+const cards = (state, action) => {
+	switch(action.type) {
+		case 'ADD_CARD':
+			let newCard = Object.assign({}, action.data, {
+				score: 1,
+				id: +new Date
+			});
+				return state.concat([newCard]);
+			default: 
+				return state || [];
+	}
+};
+
+const store = Redux.createStore(Redux.combineReducers({
+	cards
+}));
+
+store.subscribe(function() {
+	console.log(store.getState());
+});
+
+store.dispatch({
+	type: 'ADD_CARD',
+	data: {
+		front: 'front',
+		back: 'back'
+	}
+});
+
+const App = (props) => {
+	return(<h1>Angie I love u!</h1>);	
+};
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
